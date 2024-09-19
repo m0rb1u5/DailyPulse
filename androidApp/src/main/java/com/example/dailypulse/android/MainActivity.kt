cb182bd5d26b72ce0ba946d8e65b96eced84c7d2
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2024-09-19 11:11:50 -0300

    18. Android UI with Jetpack Compose

--- androidApp/src/main/java/com/example/dailypulse/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/dailypulse/android/MainActivity.kt
@@ -8,13 +8,10 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.dailypulse.Platform
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        Platform().logSystemInfo()
 
         setContent {
             MyApplicationTheme {
@@ -22,22 +19,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView("Hello")
+                    AboutScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
 @Preview
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        AboutScreen()
     }
 }
