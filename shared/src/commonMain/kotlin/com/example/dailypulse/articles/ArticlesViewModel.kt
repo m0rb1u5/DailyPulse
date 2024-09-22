9fd3faab8677b8ccf3c43834f43d714d6f3da7fd
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2024-09-22 00:10:38 -0300

    26. Articles View Model part 2

--- shared/src/commonMain/kotlin/com/example/dailypulse/articles/ArticlesViewModel.kt
+++ shared/src/commonMain/kotlin/com/example/dailypulse/articles/ArticlesViewModel.kt
@@ -1,11 +1,24 @@
 package com.example.dailypulse.articles
 
 import com.example.dailypulse.BaseViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class ArticlesViewModel : BaseViewModel() {
     private val _articlesState: MutableStateFlow<ArticlesState> = MutableStateFlow(ArticlesState())
 
     val articlesState: StateFlow<ArticlesState> get() = _articlesState
+
+    init {
+        getArticles()
+    }
+
+    private fun getArticles() {
+        scope.launch {
+            delay(timeMillis = 500)
+            _articlesState.emit(ArticlesState())
+        }
+    }
 }
