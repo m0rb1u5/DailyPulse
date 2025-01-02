34ec033b7034dee0db3e96489575a244e11beeec
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2025-01-02 17:04:57 -0300

    51. Formatting the date

--- shared/src/commonMain/kotlin/com/example/dailypulse/articles/ArticlesUseCase.kt
+++ shared/src/commonMain/kotlin/com/example/dailypulse/articles/ArticlesUseCase.kt
@@ -1,5 +1,13 @@
 package com.example.dailypulse.articles
 
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.todayIn
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.daysUntil
+import kotlinx.datetime.toLocalDateTime
+import kotlin.math.abs
+
 class ArticlesUseCase(private val articlesService: ArticlesService) {
     suspend fun getArticles(): List<Article> {
         val articlesRaw = articlesService.fetchArticles()
@@ -10,12 +18,24 @@ class ArticlesUseCase(private val articlesService: ArticlesService) {
         Article(
             title = raw.title,
             desc = raw.desc ?: "Click to find out more",
-            date = raw.date,
-            imageUrl = raw.imageUrl ?: "https://archive.org/download/placeholder-image/placeholder-image.jpg",
+            date = getDaysAgoString(raw.date),
+            imageUrl = raw.imageUrl
+                ?: "https://archive.org/download/placeholder-image/placeholder-image.jpg",
         )
     }
 
     private fun getDaysAgoString(date: String): String {
+        val today = Clock.System.todayIn(TimeZone.currentSystemDefault())
+        val days = today.daysUntil(
+            Instant.parse(date).toLocalDateTime(TimeZone.currentSystemDefault()).date
+        )
 
+        val result = when {
+            abs(days) > 1 -> "${abs(days)} days ago"
+            abs(days) == 1 -> "yesterday"
+            else -> "today"
+        }
+
+        return result
     }
 }
