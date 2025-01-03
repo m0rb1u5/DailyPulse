f0b7b92b3977008c0a9ee29bccb1b803af686598
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2025-01-03 14:42:43 -0300

    58. Initialize Koin in Android

--- androidApp/src/main/java/com/example/dailypulse/android/di/ViewModelsModule.kt
+++ androidApp/src/main/java/com/example/dailypulse/android/di/ViewModelsModule.kt
@@ -4,6 +4,6 @@ import com.example.dailypulse.articles.ArticlesViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
-val ViewModelsModule = module {
+val viewModelsModule = module {
     viewModel { ArticlesViewModel(get()) }
 }
